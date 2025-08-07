e3a33eb13212a4e7df75ccf8adf804d8b862d0eb
giordo <giordogi@gmail.com>
2025-08-07 18:48:03 +0200

    other files added

--- src/app.js
+++ src/app.js
@@ -1,51 +1,19 @@
  
-    var mkcontent = "";
+window.addEventListener("load", (event) => {
+    const openModal = document.querySelector("#open-modal");
+    const modal = document.querySelector(".modal");
+    const modalContainer = document.querySelector(".modal-container");
+    const body = document.querySelector("body");
+    const closebtn = modal.querySelector('#close-modal');
 
-    $("#test-openmodal").click(function() {
-        //cont = build();
-        OpenDialogForSelectionAdmItem('Prodottogiordo', 'ciao', function() {});
+    openModal.addEventListener("click", () => {
+    modalContainer.classList.add("show-container");
+    modal.classList.add("show");
+    body.style.overflowY = "hidden";
     });
 
-    function OpenDialogForSelectionAdmItem(title, content, callback) {
-        var dlg = new BootstrapDialog({
-            title: title,
-            message: content,
-            onshow: function(dialog){
-                if($('.modal-dialog').length > 0){
-                    w = parseInt($('.modal-dialog:eq(-1)').css('width')) - 50;
-                    dialog.$modalDialog.css('width',  w + 'px');
-                }
-            },
-            onshown: function(dialog) {
-                if (document.activeElement) {
-                    document.activeElement.blur();
-                }  
-                var tier = $('.bootstrap-dialog').length - 1;
-                dialog.$modal.prev(".modal-backdrop")
-                        .css("z-index", 1030 + tier * 30);
-                dialog.$modal
-                        .css("z-index", 1040 + tier * 30);
-
-            },
-            onhide: function(dialog){                
-                dialog.$modal.html("");
-            },
-            buttons: [{
-                label: 'Save',
-                cssClass: 'btn-primary',
-                id: 'btnSave',
-                action: function (dialog) {
-                    if (callback !== "") { callback(); }
-                    dialog.close();
-                }
-            },{
-                label: 'Close',
-                cssClass: 'btn',
-                id: 'btnClose',
-                action: function (dialog) {                   
-                    dialog.close();
-                }
-            }
-            ]
-        }).open();
-    }
+    closebtn.addEventListener("click",()=>{
+        modalContainer.classList.remove("show-container");
+        body.style.overflowY = "visible";
+    });
+});
